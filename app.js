49a9a5c2645ab22402e948b601fe8caa933f3c48
Mike Richter <richtercloud@outlook.com>
2017-08-03 20:43:05 -0400

    finished core features

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-
+var sb = require('./sb');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat connector for communicating with the Bot Framework Service
@@ -18,16 +18,48 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+    session.beginDialog('helloDialog');
 });
 
 // Add a global LUIS recognizer to the bot by using the endpoint URL of the LUIS app
 var model = 'https://eastus2.api.cognitive.microsoft.com/luis/v2.0/apps/e86551ad-3835-4eba-a0a7-f9b9baedca75?subscription-key=a9f1a1f4d2324203b0ea638d2a9fad76&verbose=true&timezoneOffset=0&q=';
 bot.recognizer(new builder.LuisRecognizer(model));
-//bot.recognizer(new builder.RegExpRecognizer( 'Url', { en_us: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/ }));
-bot.dialog('helloDialog', function (session) {
-    session.endDialog("Hey! Welcome to the OCR Bot. Send me a URL to an image and I will email it to you.");
-}).triggerAction({ matches: 'Hello' });
+bot.dialog('helloDialog', [
+    function (session) {
+        if (session.userData.email) {
+            session.send('Hey %s, enter a URL and I\'ll send you the text!');
+            session.endDialog('Let me know if you want to change your email address.');
+        }
+        else {
+            session.send('Hey buddy! Welcome to the OCR bot. Send me a URL and I will email you the text.');
+            session.beginDialog('emailDialog');
+        }
+    }
+]);
+
+bot.dialog('emailDialog', [
+    function (session) {
+        builder.Prompts.text(session, 'what is your email address?')
+    },
+    function (session, results) {
+        session.userData.email = results.response;
+        session.endDialog('Thanks, I will send emails to *%s*', session.userData.email);
+    }
+]).triggerAction({ matches: 'Email' });
+
+bot.dialog('noneDialog', [
+    function (session) {
+        if (session.userData.email) {
+            session.endDialog('Hey %s! You can change your email or send me a URL!', session.userData.email);
+        }
+        else {
+            session.send('Hey buddy! Welcome to the OCR bot. Send me a URL and I will email you the text.');
+            session.beginDialog('emailDialog');
+        }
+    }
+]).triggerAction({ matches: 'None' });
+
 bot.dialog('urlDialog', function (session) {
+    sb(session.userData.email, session.message.text);
     session.endDialog("I like that URL!");
 }).triggerAction({ matches: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/ });
--- sb.js
+++ sb.js
@@ -4,12 +4,17 @@ var azure = require('azure');
 //var serviceBusService = azure.createServiceBusService();
 var serviceBusService = azure.createServiceBusService('Endpoint=sb://botbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=u7ORBFDotue0j3xj7WZj5qJpQrhmXKa5XekWRO1M5mc=');
 
-var messageBody = {
-    imageUrl: process.argv[2]
-};
 
-serviceBusService.sendTopicMessage('usertopic', JSON.stringify(messageBody), function (error) {
-    if (error) {
-        console.log(error);
-    }
-});
+
+
+module.exports = function (email, url) {
+    var messageBody = {
+        email: email,
+        imageUrl: url
+    };
+    serviceBusService.sendTopicMessage('usertopic', JSON.stringify(messageBody), function (error) {
+        if (error) {
+            console.log(error);
+        }
+    });
+};
